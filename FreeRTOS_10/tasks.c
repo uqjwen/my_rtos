89b00a6bb3e7f7e36d987a685e78e116a1568145
uqjwen <wen_jiahui@outlook.com>
2019-12-08 22:02:06 +0800

    wenjh

--- FreeRTOS_10/tasks.c
+++ FreeRTOS_10/tasks.c
@@ -3274,7 +3274,7 @@ static portTASK_FUNCTION( prvIdleTask, pvParameters )
 		vPortStoreTaskMPUSettings( &( pxTCB->xMPUSettings ), xRegions, NULL, 0 );
 	}
 
-#endif /* portUSING_MPU_WRAPPERS */
+#endif /* portUSING_MPU_WRAPPERS */  
 /*-----------------------------------------------------------*/
 
 static void prvInitialiseTaskLists( void )
